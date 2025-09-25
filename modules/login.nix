0b993ba7c2f9ae554ef4f1286ebc4a3b9867f1b2
Zaraki <walter@pariggers.com>
2025-09-25 17:46:35 +0200

    login working

--- modules/login.nix
+++ modules/login.nix
@@ -1,17 +1,40 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  # Hyprland session handling
+  #### UWSM + Hyprland session ####
   programs.uwsm.enable = true;
+  programs.uwsm.waylandCompositors.hyprland = {
+    prettyName = "Hyprland";
+    comment   = "Hyprland compositor managed by UWSM";
+    binPath   = "/run/current-system/sw/bin/Hyprland";
+  };
 
-  # Lightweight login manager
+  #### Greeter (greetd + regreet) ####
   services.greetd.enable = true;
-  programs.regreet.enable = true;
 
-  services.greetd.settings = {
-    default_session = {
-      command = "regreet";
-      user = "greeter";
+  programs.regreet = {
+    enable = true;
+
+    # Minimal config: just wallpaper + clock (+ a few env niceties)
+    settings = {
+      prefer_dark = true;
+
+      clock = {
+        enabled = true;
+        format = "%A, %d %B %Y  %H:%M";
+      };
+
+      env = {
+        NIXOS_OZONE_WL      = "1";
+        QT_QPA_PLATFORM     = "wayland";
+        XDG_CURRENT_DESKTOP = "Hyprland";
+      };
     };
+  };
+
+  # Run ReGreet inside cage so GTK has a Wayland compositor
+  services.greetd.settings.default_session = {
+    user = "greeter";
+    command = "${pkgs.cage}/bin/cage -s -- ${config.programs.regreet.package}/bin/regreet";
   };
 }
