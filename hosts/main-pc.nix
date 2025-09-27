23422cbb55f59c913869673bedc478a23ca2533f
Zaraki <walter@pariggers.com>
2025-09-27 20:30:51 +0200

    formating

--- hosts/main-pc.nix
+++ hosts/main-pc.nix
@@ -8,20 +8,18 @@
   ];
   services.printing.enable = true;
 
+  home-manager.backupFileExtension = "hm-bak";
   hardware.printers = {
-    ensurePrinters = [
-      {
-        name        = "brother-hl-l8260cdw";
-        description = "Brother HL-L8260CDW (Wi-Fi)";
-        deviceUri   = "ipp://192.168.0.9:631/ipp/print";
-        model       = "everywhere";        # IPP Everywhere (driverless)
-        # ppdOptions = { Duplex = "DuplexNoTumble"; };  # optional tweaks
-      }
-  ];
+    ensurePrinters = [{
+      name = "brother-hl-l8260cdw";
+      description = "Brother HL-L8260CDW (Wi-Fi)";
+      deviceUri = "ipp://192.168.0.9:631/ipp/print";
+      model = "everywhere"; # IPP Everywhere (driverless)
+      # ppdOptions = { Duplex = "DuplexNoTumble"; };  # optional tweaks
+    }];
 
     ensureDefaultPrinter = "brother-hl-l8260cdw";
   };
-
 
   # Hyprland
   programs.hyprland.enable = true;
@@ -30,7 +28,7 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-  
+
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
